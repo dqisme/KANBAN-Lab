a2adabe4b5a0a23bb735035d344f554457cec5e0
dqisme <dqisme@outlook.com>
2016-06-17 23:46:17 +0800

    [dq] #N/A replace string path with path module joining

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
+var path = require('path');
+
 module.exports = {
-  entry: './src/index.ts',
+  entry: path.join(__dirname, 'src', 'index.ts'),
   output: {
-    path: __dirname + '/dist',
+    path: path.join(__dirname, 'dist'),
     filename: 'index.js'
   },
   module: {
     loaders: [
       {
         test: /\.ts$/,
+        include: path.join(__dirname, 'src'),
         loaders: ['ts']
       }
     ]
   },
   devServer: {
-    contentBase: 'dist/',
+    contentBase: path.join(__dirname, 'dist'),
     host: '0.0.0.0'
   }
 };
