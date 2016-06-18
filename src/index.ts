e6332294d47569a4de519f7064b1682f38421e9f
dqisme <dqisme@outlook.com>
2016-06-18 21:38:18 +0800

    [dq] #N/A first app module with controller

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 /// <reference path="../typings/index.d.ts" />
-require("angular");
+const angular = require("angular");
 require("./index.jade");
+
+const app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+  $scope.firstName = "John";
+  $scope.lastName = "Dong";
+});
